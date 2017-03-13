0ffb69cfd603de1627b846bcb46f7a06a79e25a3
Gleb <mail@g1eb.com>
2017-03-13 12:24:42 +0100

    Check if file is already linked otherwise print error message.

--- link.sh
+++ link.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 for file in .*
 do
-  if [[ $file =~ ^.[^(.|git)] ]];
-    then ln -s "$PWD/$file" "$HOME/$file";
+  if [[ $file =~ ^.[^(.|git)] ]]; then
+    err=$(ln -s "$PWD/$file" "$HOME/$file" 2>&1);
+    if [[ ! "$PWD/$file" -ef "$HOME/$file" ]];
+      then printf >&2 '%s\n' "$err";
+    fi
   fi;
 done
