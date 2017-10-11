a90d4988c00f25b91842f235fcc1bdecce35e6e2
g1eb <mail@g1eb.com>
2017-10-11 15:20:25 +0000

    Updated link script to match all dotfiles except the '.git' folder.

--- link.sh
+++ link.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-for file in .*
+for file in $(ls -a1 | grep -P '^\.(?!git$|\.).+$')
 do
-  if [[ $file =~ ^.[^(.|git)] ]]; then
-    err=$(ln -s "$PWD/$file" "$HOME/$file" 2>&1);
-    if [[ ! "$PWD/$file" -ef "$HOME/$file" ]];
-      then printf >&2 '%s\n' "$err";
-    fi
-  fi;
+  err=$(ln -s "$PWD/$file" "$HOME/$file" 2>&1);
+  if [[ ! "$PWD/$file" -ef "$HOME/$file" ]];
+    then printf >&2 '%s\n' "$err";
+  fi
 done
